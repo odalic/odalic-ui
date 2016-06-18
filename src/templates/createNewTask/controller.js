b258c2164fab6c193baa86b51afd0d3b033a8b00
Kata.Boku <Kata.Boku@seznam.cz>
2016-06-18 08:55:00 +0200

    HTML form for the creation of new task

---
+++ src/templates/createNewTask/controller.js
@@ -0,0 +1,11 @@
+(function () {
+
+    // Main module
+    var app = angular.module('odalic-app');
+
+    // Create a controller for taskconfig
+    app.controller('createNewTask-ctrl', function ($scope) {
+        $scope.savedFiles = ["Emil", "Tobias", "Linus"];
+    });
+
+})();
