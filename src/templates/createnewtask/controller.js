3600ded7d0d08814bd20518bca3d84782f61b799
Istvan Satmari <istvan.satmari@gmail.com>
2016-12-16 19:00:19 +0100

    Closes #22

--- src/templates/createnewtask/controller.js
+++ src/templates/createnewtask/controller.js
@@ -3,10 +3,8 @@
     // Main module
     var app = angular.module('odalic-app');
 
-    // Settings
-    var settings = {
-        fileProvisionTypes: ['local', 'remote']
-    };
+    // Load submodules
+    loadhelp.loadDefault();
 
     // Create a controller for task-creation screen
     app.controller('createnewtask-ctrl', function ($scope, $routeParams, filedata, rest, report) {
@@ -23,21 +21,11 @@
             creating: true
         };
 
-        // Files
-        $scope.fileProvision = settings.fileProvisionTypes[0];
-        $scope.files = {};
-        $scope.remoteFile = {};
+        $scope.fileinput = {};
 
-        // Static alerts
         $scope.alerts = {
             taskCreation: {
                 identifier: {
-                    type: 'error',
-                    visible: false
-                }
-            },
-            files: {
-                selectedFile: {
                     type: 'error',
                     visible: false
                 }
@@ -56,152 +44,6 @@
         $scope.automaticSelectPrimaryKB = function () {
             $scope.primaryKB = $scope.chosenKBs[0];
         };
-
-        // File uploading
-        $scope.fileUpload = {
-            // Id of the input-file element
-            inputFileId: 'concreteFile',
-
-            // Are we uploading a file at the moment?
-            uploadingFile: false,
-
-            // Button for file uploading disabled?
-            isUploadDisabled: true,
-
-            // Identifier of the file to be uploaded
-            identifier: String(),
-
-            // Messages for a user
-            alerts: [],
-
-            // List of uploaded files
-            uploaded: [],
-
-            // Pushing an alert message for 'file upload'
-            pushAlert: function (type, text) {
-                var _ref = this;
-                _ref.alerts.push({
-                    type: type,
-                    visible: true,
-                    text: text,
-                    close: function () {
-                        _ref.alerts.splice(_ref.alerts.indexOf(this), 1);
-                    }
-                });
-            },
-
-            // Automatically fill the 'identifier' when a file is selected
-            fillIdentifier: function () {
-                var name = filedata.fileName(this.inputFileId);
-                this.isUploadDisabled = !name;
-                if (!name) {
-                    name = text.randomId();
-                }
-
-                this.identifier = name;
-                $scope.$apply();
-            },
-
-            // Refresh the list of uploaded files
-            refreshUploaded: function (callback) {
-                var _ref = this;
-                rest.files.list.exec(
-                    // Success
-                    function (response) {
-                        _ref.uploaded = response;
-                        if (callback) {
-                            callback(true);
-                        }
-                    },
-                    // Failure
-                    function failure(response) {
-                        if (callback) {
-                            callback(false);
-                        }
-                    }
-                );
-            },
-
-            // Validates the file uploading part of the form; returns true, if it is safe to proceed with the file upload
-            validate: function () {
-                // Clear previous alerts
-                this.alerts = [];
-                var valid = true;
-
-                // Local file selected?
-                if (!filedata.filePresent(this.inputFileId)) {
-                    this.pushAlert('error', 'No local file selected for the upload.');
-                    valid = false;
-                }
-
-                // Identifier set?
-                if (!this.identifier) {
-                    this.pushAlert('error', 'Identifier must not be empty.');
-                    valid = false;
-                }
-
-                return valid;
-            },
-
-            // Upload the selected file
-            uploadFile: function () {
-                // Validate
-                if (!this.validate()) {
-                    return;
-                }
-
-                // Reference to self
-                var _ref = this;
-
-                // The file is now uploading. Hide the 'upload' button to prevent multiple uploads.
-                _ref.uploadingFile = true;
-
-                // Uploading the file asynchronously
-                sendData = function (fileData) {
-                    rest.files.name(_ref.identifier).create.upload(filedata.fileObject(_ref.inputFileId)).exec(
-                        // Success
-                        function () {
-                            // The file has been uploaded successfully => refresh the list of available files
-                            _ref.refreshUploaded(function (succes) {
-                                // Display a success message
-                                _ref.pushAlert('success', 'The file has been successfully uploaded.');
-
-                                // Sets the newly uploaded file as the selected one
-                                var uploadedFileIndex = _ref.uploaded.map(function (file) {
-                                    return file.id;
-                                }).indexOf(_ref.identifier);
-                                $scope.files.selectedFile = _ref.uploaded[uploadedFileIndex];
-
-                                // Clear the fields
-                                _ref.identifier = String();
-                                filedata.clearInputFile("concreteFile");
-
-                                // Another file may be uploaded again
-                                _ref.uploadingFile = false;
-                                _ref.isUploadDisabled = true;
-
-                                // Clear chosen file
-                                angular.element("input[type='file']").val(null);
-                            });
-                        },
-                        // Failure
-                        function (response) {
-                            // The file has not been uploaded => display an error message
-                            _ref.pushAlert('error', 'An error occured while uploading the file. Details: ' + text.dotted(response, 50));
-
-                            // A file may be uploaded again
-                            _ref.uploadingFile = false;
-                        }
-                    );
-                };
-
-                // Read the file and send the data to server
-                filedata.readBase64(_ref.inputFileId, function (fileData) {
-                    sendData(fileData);
-                });
-            }
-        };
-        $scope.fileUpload.refreshUploaded();
 
         $scope.wholeForm = {
             // Messages for a user
@@ -235,9 +77,9 @@
                 // File selected?
                 switch ($scope.fileProvision) {
                     case 'local':
-                        if (!objhelp.objRecurAccess($scope, 'files', 'selectedFile')['id']) {
+                        if (!$scope.fileinput.isFileSelected()) {
                             valid = false;
-                            $scope.alerts.files.selectedFile.visible = true;
+                            $scope.fileinput.pushAlert('error', 'No file selected.');
                         }
                         break;
                     case 'remote':
@@ -260,7 +102,7 @@
             }
 
             // Generic preparations
-            var fileId = $scope.files.selectedFile.id;
+            var fileId = $scope.fileinput.getSelectedFile();
             var taskId = $scope.taskCreation.identifier;
 
             // TODO: A loading icon should be displayed until the task is actually inserted on the server. If an error arises a tooltip / alert should be displayed.
@@ -336,7 +178,7 @@
             }
 
             // Generic preparations
-            var fileId = $scope.files.selectedFile.id;
+            var fileId = $scope.fileinput.getSelectedFile();
 			var taskid = $scope.taskCreation.identifier;
 
             // TODO: A loading icon should be displayed until the task is actually inserted on the server. If an error arises a tooltip / alert should be displayed.
@@ -381,19 +223,12 @@
                     // Success
                     function (response) {
                         // Find the previously chosen file for the current task
-                        var selectedFile = 0;
-                        var uploaded = $scope.fileUpload.uploaded;
-                        for (var i = 0; i < uploaded.length; ++i) {
-                            if (uploaded[i].id === response.configuration.input) {
-                                selectedFile = i;
-                                break;
-                            }
-                        }
+                        timed.ready(function () { return !!$scope.fileinput.setSelectedFile; }, function () {
+                            $scope.fileinput.setSelectedFile(response.configuration.input);
+                        });
 
                         // Fill the controls
                         objhelp.objRecurAccess($scope, 'taskCreation')['identifier'] = response.id;
-                        $scope.fileProvision = 'local';
-                        $scope.files.selectedFile = $scope.fileUpload.uploaded[selectedFile];
                         $scope.taskCreation.description = response.description;
                         $scope.templFormat.creating = false;
                     },
@@ -405,20 +240,6 @@
                 );
             }
         })();
-
-
-        $scope.sendTest = function () {
-            rest.files.name($scope.fileUpload.identifier).create.remote($scope.remoteFile.location).exec(
-                // Success
-                function (response) {
-                    console.log(response);
-                },
-                // Failure
-                function (response) {
-                    console.log(response);
-                }
-            );
-        };
 
     });
 
---
+++ src/templates/createnewtask/fileinput/fileinput.js
@@ -0,0 +1,308 @@
+(function () {
+
+    // Main module
+    var app = angular.module('odalic-app');
+
+    var currentFolder = $.getPathForRelativePath('');
+    app.directive('fileinput', function (rest, filedata) {
+        return {
+            restrict: 'E',
+            scope: {
+                bind: '='
+            },
+            templateUrl: currentFolder + 'fileinput.html',
+            link: function (scope, iElement, iAttrs) {
+                // Initialization
+                scope.files = {};
+                scope.remoteFile = {};
+
+                // Alerts (messages for a user)
+                scope.messages = {
+                    // Messages to display
+                    alerts: [],
+
+                    // Text messages injected from template
+                    txt: {},
+
+                    // Pushing an alert message
+                    push: function (type, text) {
+                        var _ref = this;
+                        _ref.alerts.push({
+                            type: type,
+                            visible: true,
+                            text: text,
+                            close: function () {
+                                _ref.alerts.splice(_ref.alerts.indexOf(this), 1);
+                            }
+                        });
+                    },
+
+                    // Clear any previous alerts
+                    clear: function () {
+                        this.alerts = [];
+                    }
+                };
+
+                // File list
+                scope.fileList = {
+                    // List of (uploaded or attached) file identifiers
+                    identifiers: [],
+
+                    // Refresh the list of uploaded files
+                    refresh: function (callback) {
+                        var _ref = this;
+                        rest.files.list.exec(
+                            // Success
+                            function (response) {
+                                _ref.identifiers = response;
+                                if (callback) {
+                                    callback(true);
+                                }
+                            },
+                            // Failure
+                            function failure(response) {
+                                if (callback) {
+                                    callback(false);
+                                }
+                            }
+                        );
+                    },
+
+                    getIndex: function (identifier) {
+                        var index = null;
+                        for (var i = 0; i < this.identifiers.length; i++) {
+                            if (this.identifiers[i].id === identifier) {
+                                index = i;
+                                break;
+                            }
+                        }
+                        return index;
+                    },
+
+                    setSelected: function (index) {
+                        this.selectedFile = this.identifiers[index];
+                        // scope.$apply();
+                    }
+                };
+
+                // File uploading
+                scope.fileUpload = {
+                    // Id of the input-file element
+                    inputFileId: 'concreteFile',
+
+                    // Are we uploading a file at the moment?
+                    uploadingFile: false,
+
+                    // Button for file uploading disabled?
+                    isUploadDisabled: true,
+
+                    // Identifier of the file to be uploaded
+                    identifier: String(),
+
+                    // Automatically fill the 'identifier' when a file is selected
+                    fillIdentifier: function () {
+                        var name = filedata.fileName(this.inputFileId);
+                        this.isUploadDisabled = !name;
+                        if (!name) {
+                            name = text.randomId();
+                        }
+
+                        this.identifier = name;
+                        scope.$apply();
+                    },
+
+                    // Validates the file uploading part of the form; returns true, if it is safe to proceed with the file upload
+                    validate: function () {
+                        // Clear previous alerts
+                        scope.messages.clear();
+                        var valid = true;
+
+                        // Local file selected?
+                        if (!filedata.filePresent(this.inputFileId)) {
+                            scope.messages.push('error', scope['msgtxt.noLocalFile']);
+                            valid = false;
+                        }
+
+                        // Identifier set?
+                        if (!this.identifier) {
+                            scope.messages.push('error', scope['msgtxt.emptyIdentifier']);
+                            valid = false;
+                        }
+
+                        return valid;
+                    },
+
+                    // Upload the selected file
+                    uploadFile: function () {
+                        // Validate
+                        if (!this.validate()) {
+                            return;
+                        }
+
+                        // Reference to self
+                        var _ref = this;
+
+                        // The file is now uploading. Hide the 'upload' button to prevent multiple uploads.
+                        _ref.uploadingFile = true;
+
+                        // Uploading the file asynchronously
+                        sendData = function (fileData) {
+                            rest.files.name(_ref.identifier).create.upload(filedata.fileObject(_ref.inputFileId)).exec(
+                                // Success
+                                function () {
+                                    // The file has been uploaded successfully => refresh the list of available files
+                                    scope.fileList.refresh(function (succes) {
+                                        // Display a success message
+                                        scope.messages.push('success', scope['msgtxt.uploadSuccessful']);
+
+                                        // Sets the newly uploaded file as the selected one
+                                        var uploadedFileIndex = scope.fileList.identifiers.map(function (file) {
+                                            return file.id;
+                                        }).indexOf(_ref.identifier);
+                                        scope.fileList.selectedFile = scope.fileList.identifiers[uploadedFileIndex];
+
+                                        // Clear the fields
+                                        _ref.identifier = String();
+                                        filedata.clearInputFile(_ref.inputFileId);
+
+                                        // Another file may be uploaded again
+                                        _ref.uploadingFile = false;
+                                        _ref.isUploadDisabled = true;
+
+                                        // Clear chosen file
+                                        angular.element("input[type='file']").val(null);
+                                    });
+                                },
+                                // Failure
+                                function (response) {
+                                    // The file has not been uploaded => display an error message
+                                    scope.messages.push('error', String.concat(
+                                        scope['msgtxt.uploadFailure'], ' ',
+                                        scope['msgtxt.errorDescription'], ' ',
+                                        text.dotted(JSON.parse(response.data).payload.text, 50)
+                                    ));
+
+                                    // A file may be uploaded again
+                                    _ref.uploadingFile = false;
+                                }
+                            );
+                        };
+
+                        // Read the file and send the data to server
+                        filedata.readBase64(_ref.inputFileId, function (fileData) {
+                            sendData(fileData);
+                        });
+                    }
+                };
+
+                // Remote file attaching
+                scope.fileAttach = {
+                    // Are we attaching a file at the moment?
+                    attachingFile: false,
+
+                    // Identifier of the file to be attached
+                    identifier: String(),
+
+                    // Location of the file to be attached
+                    location: String(),
+
+                    // Validates the file attaching part of the form; returns true, if it is safe to proceed with the file attach
+                    validate: function () {
+                        // Clear previous alerts
+                        scope.messages.clear();
+                        var valid = true;
+
+                        // Identifier set?
+                        if (!this.identifier) {
+                            scope.messages.push('error', scope['msgtxt.emptyIdentifier']);
+                            valid = false;
+                        }
+
+                        return valid;
+                    },
+
+                    // Attach the selected file
+                    attachFile: function () {
+                        // Validate
+                        if (!this.validate()) {
+                            return;
+                        }
+
+                        // Reference to self
+                        var _ref = this;
+
+                        // The file is now attaching. Hide the 'attach' button to prevent multiple attachments.
+                        _ref.attachingFile = true;
+
+                        // Send the REST request
+                        rest.files.name(_ref.identifier).create.remote(_ref.location).exec(
+                            // Success
+                            function () {
+                                // The file has been attached successfully => refresh the list of available files
+                                scope.fileList.refresh(function (succes) {
+                                    // Display a success message
+                                    scope.messages.push('success', scope['msgtxt.attachSuccessful']);
+
+                                    // Sets the newly attached file as the selected one
+                                    var attachedFileIndex = scope.fileList.identifiers.map(function (file) {
+                                        return file.id;
+                                    }).indexOf(_ref.identifier);
+                                    scope.fileList.selectedFile = scope.fileList.identifiers[attachedFileIndex];
+
+                                    // Clear the fields
+                                    _ref.identifier = String();
+                                    _ref.location = scope['deflocation'];
+
+                                    // Another file may be uploaded again
+                                    _ref.attachingFile = false;
+                                });
+                            },
+                            // Failure
+                            function (response) {
+                                // The file has not been attached => display an error message
+                                scope.messages.push('error', String.concat(
+                                    scope['msgtxt.attachFailure'], ' ',
+                                    scope['msgtxt.errorDescription'], ' ',
+                                    text.dotted(JSON.parse(response.data).payload.text, 50)
+                                ));
+
+                                // A file may be uploaded again
+                                _ref.attachingFile = false;
+                            }
+                        );
+                    }
+                };
+
+                // Additional actions
+                scope.fileList.refresh();
+
+                // Public interface
+                scope.bind.isFileSelected = function () {
+                    var fl = scope.fileList;
+                    return !!fl.selectedFile && !!fl.selectedFile.id;
+                };
+
+                scope.bind.getSelectedFile = function () {
+                    if (!scope.bind.isFileSelected()) {
+                        throw new Error('fileinput component: No file selected.');
+                    }
+                    return scope.fileList.selectedFile.id;
+                };
+
+                scope.bind.setSelectedFile = function (id) {
+                    if (typeof(id) === 'string') {
+                        var index = scope.fileList.getIndex(id);
+                        scope.fileList.setSelected(index);
+                    } else {
+                        throw new Error('fileinput component: Unsupported argument id.');
+                    }
+                };
+
+                scope.bind.pushAlert = function(type, text) {
+                    scope.messages.push(type, text);
+                };
+            }
+        }
+    });
+
+})();
