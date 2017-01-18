6a1711ae3b3f840f04e29b8f43a2ed8ab809b837
Istvan Satmari <istvan.satmari@gmail.com>
2017-01-18 19:20:43 +0100

    satellizer initial set-up

---
+++ src/services/authentication.js
@@ -0,0 +1,26 @@
+(function () {
+
+    // Main module
+    var app = angular.module('odalic-app');
+
+    // Configure routes
+    app.config(function ($authProvider) {
+
+        $authProvider.google({
+            clientId: '526771567449-ope1mlfqrptemsqbgk4iae1e9j6728kf.apps.googleusercontent.com',
+            url: 'authtest/index.htm',
+            redirectUri: window.location.origin,
+            responseType: 'token'
+        });
+
+        // Custom
+        // $authProvider.oauth2({
+        //     name: 'foursquare',
+        //     url: '/auth/foursquare',
+        //     clientId: 'Foursquare Client ID',
+        //     redirectUri: window.location.origin,
+        //     authorizationEndpoint: 'https://foursquare.com/oauth2/authenticate',
+        // });
+    });
+
+})();
--- src/templates/ngtest/controller.js
+++ src/templates/ngtest/controller.js
@@ -5,7 +5,51 @@
 
     // Create a controller for ngtest
     var currentFolder = $.getPathForRelativePath('');
-    app.controller('odalic-ngtest-ctrl', function ($scope) {
+    app.controller('odalic-ngtest-ctrl', function ($scope, $auth, $http) {
+
+        $scope.myauth = function() {
+            $auth.authenticate('google')
+                .then(function(response) {
+                    console.log('Connected!');
+                    console.log(response);
+                })
+                .catch(function(response) {
+                    console.log('Failure!');
+                    console.log(response);
+                });
+        };
+
+        $scope.testauth = function() {
+            console.log($auth.isAuthenticated());
+            console.log($auth.getToken());
+            console.log($auth.getPayload());
+        };
+        
+        $scope.testconn = function () {
+            // TODO: Everything needs to be rewritten to use "$http" as its 'ajax-middleware' in rest services
+            $http({
+                method: 'GET',
+                url: '/meh'
+                //skipAuthorization: true
+            });
+        };
+
+        $scope.basicauth = function () {
+            var user = {
+                email: 'istvan.satmari@gmail.com',
+                password: 'example'
+            };
+
+            $auth.login(user)
+                .then(function(response) {
+                    console.log('login successful!');
+                    console.log(response);
+                })
+                .catch(function(response) {
+                    console.log('login failure!');
+                    console.log(response);
+                });
+        };
 
     });
 
