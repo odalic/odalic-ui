5ee47fbdfa6b2ecbb7dda6130b85dec173a759d7
Istvan Satmari <istvan.satmari@gmail.com>
2017-02-05 16:14:25 +0100

    Closes #249 (forgot taskimport confirm dialog)

--- src/templates/importtask/controller.js
+++ src/templates/importtask/controller.js
@@ -8,6 +8,7 @@
 
         // Initialization
         formsval.toScope($scope);
+        $scope.confirm = {};
 
         // Form controls
         $scope.taskImport = {
@@ -26,24 +27,46 @@
                 var taskId = $scope.taskImport.identifier;
 
                 // Send REST request for task import
-                rest.tasks.name(taskId).configuration.import(filedata.fileObject('concreteFile')).exec(
-                    // Success
-                    function (response) {
-                        // Don't handle if further action was specified
-                        if (callback) {
-                            callback();
-                            return;
+                var process = function () {
+                    rest.tasks.name(taskId).configuration.import(filedata.fileObject('concreteFile')).exec(
+                        // Success
+                        function (response) {
+                            // Don't handle if further action was specified
+                            if (callback) {
+                                callback();
+                                return;
+                            }
+
+                            // The task has been created, redirect to the task configurations screen
+                            window.location.href = text.urlConcat('#/taskconfigs/', taskId);
+                        },
+
+                        // Failure
+                        function (response) {
+                            $scope.taskImport.alerts.push('error', reporth.constrErrorMsg($scope['msgtxt.createFailure'], response.data));
+                            f();
                         }
+                    );
+                };
 
-                        // The task has been created, redirect to the task configurations screen
-                        window.location.href = text.urlConcat('#/taskconfigs/', taskId);
+                // Insert the task, if everything is OK
+                rest.tasks.name(taskId).exists(
+                    // The task already exists => confirm overwrite
+                    function () {
+                        $scope.confirm.open(function (response) {
+                            if (response === true) {
+                                process();
+                            } else {
+                                f();
+                                // Clicking outside of the modal is not registered by angular, but clicking on the modal button is => manually call digest cycle if necessary
+                                if (!$scope.$$phase) {
+                                    $scope.$apply();
+                                }
+                            }
+                        });
                     },
-
-                    // Failure
-                    function (response) {
-                        $scope.taskImport.alerts.push('error', reporth.constrErrorMsg($scope['msgtxt.createFailure'], response.data));
-                        f();
-                    }
+                    // The task does not exist yet => create without any prompt
+                    process
                 );
             },
 
