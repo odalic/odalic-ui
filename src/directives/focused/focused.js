32129fcf2a400893af82e35dccfffe3a6cf2e1c6
Istvan Satmari <istvan.satmari@gmail.com>
2017-02-10 16:06:29 +0100

    Closes #226

---
+++ src/directives/focused/focused.js
@@ -0,0 +1,49 @@
+(function () {
+
+    // Main module
+    var app = angular.module('odalic-app');
+
+    /** Making input control focused.
+     */
+    app.directive('focused', function () {
+        return {
+            restrict: 'A',
+            scope: {
+                model: '=focused'
+            },
+            link: function (scope, iElement, iAttrs) {
+
+                // Behaviour depending on the attribute 'model'
+                if (typeof(scope.model) !== 'undefined') {
+                    // Initialization
+                    var focusfn = function (focused) {
+                        if (focused === true) {
+                            window.setTimeout(function () {
+                                iElement[0].focus();
+                            });
+                        }
+                    };
+
+                    // Watch for the corresponding value
+                    scope.$watch(scope.model, function (value) {
+                        console.log('value=', value);
+                        focusfn(value);
+                    });
+
+                    // On losing focus change the value
+                    iElement.bind('blur', function () {
+                        scope.$apply(scope.model.assign(scope, false));
+                    });
+
+                    // Start focused?
+                    focusfn(scope.model);
+                } else {
+                    // Set focus automatically if the attribute is not set
+                    iElement[0].focus();
+                }
+
+            }
+        }
+    });
+
+})();
