a32d09850b75b92ba05b4cb7e6394a2ec4a27e59
Kata.Boku <Kata.Boku@seznam.cz>
2017-02-16 22:02:25 +0100

    closes #364 and closes#342

--- src/templates/taskresult/controller.js
+++ src/templates/taskresult/controller.js
@@ -200,26 +200,12 @@
                 function (response) {
                     $scope.result = response;
 
-                    // Prepare data for graphvis component
-                    actions.push(setsData);
+                    // Prepare data for graphvis component and data cube
+                    actions.push(function () {
+                        setsData();
+                        setsDataCube();
+                    });
 
-                    //TODO jak pockat i na inputfile
-                    if ($scope.statistical == true) {
-                        for (var index in $scope.result.statisticalAnnotations) {
-                            var predicateObj = $scope.result.statisticalAnnotations[index];
-                            var predicate = predicateObj.predicate;
-
-                            predicate.candidates = {};
-                            predicate.candidates[$scope.primaryKB] = angular.copy(predicate[$scope.primaryKB]);
-                            predicate.chosen = {};
-                            predicate.chosen[$scope.primaryKB] = angular.copy(predicate[$scope.primaryKB]);
-
-
-                            var header = $scope.inputFile.columns[index];
-                            predicateObj.label = header;
-                            predicateObj.index = index;
-                        }
-                    }
 
                     // Phase complete
                     dataLoaded(phases.result);
@@ -291,6 +277,29 @@
             );
             //endregion
         })();
+
+        //prepares json for page with data cube
+        setsDataCube = function () {
+            if ($scope.statistical == true) {
+                for (var index in $scope.result.statisticalAnnotations) {
+                    var predicateObj = $scope.result.statisticalAnnotations[index];
+                    var predicate = predicateObj.predicate;
+
+                    //adds level candidate and chosen because of same interface as relation
+                    predicate.candidates = {};
+                    predicate.candidates[$scope.primaryKB] = angular.copy(predicate[$scope.primaryKB]);
+                    predicate.chosen = {};
+                    predicate.chosen[$scope.primaryKB] = angular.copy(predicate[$scope.primaryKB]);
+
+
+                    var header = $scope.inputFile.columns[index];
+                    //adds headers for data cube table
+                    predicateObj.label = header;
+                    //adds index because we need to know which item is modified
+                    predicateObj.index = index;
+                }
+            }
+        }
 
         $scope.serverFeedback = {};
 
