3017af4439186fd37278f9757ecf3f9d3b747af8
Istvan Satmari <istvan.satmari@gmail.com>
2017-04-04 13:59:20 +0200

    Small corrections of the set properties template

--- src/templates/setproperties/controller.js
+++ src/templates/setproperties/controller.js
@@ -7,14 +7,7 @@
     app.controller('odalic-setproperties-ctrl', function ($scope, $uibModal, $routeParams, filedata, rest, formsval, reporth) {
 
         // Initialize
-        $scope.labelPredicates = [
-            { id: 0, value: 'pred1' },
-            { id: 1, value: 'pred2' }
-        ];
-
-        $scope.test = function () {
-            console.log($scope.labelPredicates);
-        };
+        $scope.labelPredicates = [];
 
     });
 
