ae71184e015bd39386ecab1d6f9ec52f7b5ec461
Istvan Satmari <istvan.satmari@gmail.com>
2017-04-04 11:06:57 +0200

    KB import placeholding functionalities

--- src/templates/kbimport/controller.js
+++ src/templates/kbimport/controller.js
@@ -3,98 +3,81 @@
     // Main module
     var app = angular.module('odalic-app');
 
-    // Create a controller for task import screen
+    // Create a controller for knowledge base import screen
     app.controller('odalic-kbimport-ctrl', function ($scope, filedata, rest, formsval, reporth) {
 
-        // // Initialization
-        // formsval.toScope($scope);
-        // $scope.confirm = {};
-        //
-        // // Form controls
-        // $scope.taskImport = {
-        //     alerts: {},
-        //     identifier: new String(),
-        //
-        //     // Button "import" action
-        //     import: function (f, callback) {
-        //         // Validate
-        //         if (!formsval.validate($scope.taskImportForm)) {
-        //             f();
-        //             return;
-        //         }
-        //
-        //         // Prepare
-        //         var taskId = $scope.taskImport.identifier;
-        //
-        //         // Send REST request for task import
-        //         var process = function () {
-        //             rest.tasks.name(taskId).configuration.import(filedata.fileObject('concreteFile')).exec(
-        //                 // Success
-        //                 function (response) {
-        //                     // Don't handle if further action was specified
-        //                     if (callback) {
-        //                         callback();
-        //                         return;
-        //                     }
-        //
-        //                     // The task has been created, redirect to the task configurations screen
-        //                     window.location.href = text.urlConcat('#/taskconfigs/', taskId);
-        //                 },
-        //
-        //                 // Failure
-        //                 function (response) {
-        //                     $scope.taskImport.alerts.push('error', reporth.constrErrorMsg($scope['msgtxt.createFailure'], response.data));
-        //                     f();
-        //                 }
-        //             );
-        //         };
-        //
-        //         // Insert the task, if everything is OK
-        //         rest.tasks.name(taskId).exists(
-        //             // The task already exists => confirm overwrite
-        //             function () {
-        //                 $scope.confirm.open(function (response) {
-        //                     if (response === true) {
-        //                         process();
-        //                     } else {
-        //                         f();
-        //                         // Clicking outside of the modal is not registered by angular, but clicking on the modal button is => manually call digest cycle if necessary
-        //                         if (!$scope.$$phase) {
-        //                             $scope.$apply();
-        //                         }
-        //                     }
-        //                 });
-        //             },
-        //             // The task does not exist yet => create without any prompt
-        //             process
-        //         );
-        //     },
-        //
-        //     // Button "import and run" action
-        //     importAndRun: function (f) {
-        //         $scope.taskImport.import(f, function () {
-        //             // Prepare
-        //             var taskId = $scope.taskImport.identifier;
-        //
-        //             // Start the task
-        //             rest.tasks.name(taskId).execute.exec(
-        //                 // Execution started successfully
-        //                 function (response) {
-        //                     window.location.href = text.urlConcat('#/taskconfigs/', taskId);
-        //                 },
-        //
-        //                 // Error while starting the execution
-        //                 function (response) {
-        //                     $scope.taskImport.alerts.push('error', reporth.constrErrorMsg($scope['msgtxt.startFailure'], response.data));
-        //                     f();
-        //                 }
-        //             );
-        //         });
-        //     }
-        // };
-        //
-        // // Redirect to login screen if not logged
-        // rest.users.test.automatic.exec(objhelp.emptyFunction);
+        // Initialization
+        formsval.toScope($scope);
+        $scope.confirm = {};
+
+        // Form controls
+        $scope.kbImport = {
+            alerts: {},
+            identifier: new String(),
+
+            // Button "import" action
+            import: function (f, callback) {
+                // Validate
+                if (!formsval.validate($scope.kbImportForm)) {
+                    f();
+                    return;
+                }
+
+                // Prepare
+                var kbID = $scope.kbImport.identifier;
+
+                // TODO: Placeholder action for "import" button
+                var response = { data: { payload: { text: "This is only a DEMO." } } };
+                $scope.kbImport.alerts.push('error', reporth.constrErrorMsg($scope['msgtxt.createFailure'], response.data));
+                f();
+
+                // Send REST request for the import
+                var process = function () {
+                    // rest.tasks.name(kbID).configuration.import(filedata.fileObject('concreteFile')).exec(
+                    //     // Success
+                    //     function (response) {
+                    //         // Don't handle if further action was specified
+                    //         if (callback) {
+                    //             callback();
+                    //             return;
+                    //         }
+                    //
+                    //         // Import successful, redirect to the knowledge base configurations screen
+                    //         window.location.href = text.urlConcat('#/kblist/', kbID);
+                    //     },
+                    //
+                    //     // Failure
+                    //     function (response) {
+                    //         $scope.kbImport.alerts.push('error', reporth.constrErrorMsg($scope['msgtxt.createFailure'], response.data));
+                    //         f();
+                    //     }
+                    // );
+                };
+
+                // Insert the configuration, if everything is OK
+                // rest.tasks.name(kbID).exists(
+                //     // The configuration with the same name already exists => confirm overwrite
+                //     function () {
+                //         $scope.confirm.open(function (response) {
+                //             if (response === true) {
+                //                 process();
+                //             } else {
+                //                 f();
+                //                 // Clicking outside of the modal is not registered by angular, but clicking on the modal button is => manually call digest cycle if necessary
+                //                 if (!$scope.$$phase) {
+                //                     $scope.$apply();
+                //                 }
+                //             }
+                //         });
+                //     },
+                //     // The configuration does not exist yet => create without any prompt
+                //     process
+                // );
+            }
+        };
+
+        // Redirect to login screen if not logged
+        //rest.users.test.automatic.exec(objhelp.emptyFunction);
 
     });
 
